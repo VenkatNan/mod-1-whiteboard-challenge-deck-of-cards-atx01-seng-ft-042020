fbcd862500a28dd149ed3b74d2d03f8ba204d9e1
VenkatNan <nanditavenkat@gmail.com>
2020-06-15 09:47:49 -0500

    mod1 Card

--- solution.rb
+++ solution.rb
@@ -1,7 +1,28 @@
 class Deck
-
+    attr_reader :cards
+    def initialize
+        @cards = self.createDeck
+    end
+    def createDeck
+        res = []
+        Card.suits.each {|s| Card.ranks.each {|r| res.append(Card.new(s, r))}}
+        res
+    end
+    def choose_card
+        self.cards.slice!(rand(self.cards.count))
+    end
 end
 
 class Card
-
+    attr_reader :rank, :suit
+    def initialize(suit, rank)
+        @rank = rank
+        @suit = suit
+    end
+    def self.suits
+        ["Hearts", "Clubs", "Diamonds", "Spades"]
+    end
+    def self.ranks
+        ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    end
 end
